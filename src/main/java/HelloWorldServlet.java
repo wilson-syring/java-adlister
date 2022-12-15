e6cbc62ddb5b003d9c2956435fd0e4075d843a1c
Wilson Syring <wilson.syring@gmail.com>
2022-12-15 11:22:33 -0600

    completed sevlest into exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,12 +5,20 @@ import javax.servlet.http.*;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
+    private int counter;
+
+    public void resetCount() {
+        counter = 0;
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
+        counter++;
         PrintWriter out = response.getWriter();
         out.println("<h1>Hello, World!</h1>");
+        out.println("<h2> This page has been visited " + counter + " times! <h2>");
+
     }
 
 }
---
+++ src/main/webapp/counter.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cipher2012
+  Date: 12/15/22
+  Time: 11:00 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%! int counter = 0; %>
+<% counter += 1; %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>The current count is <%= counter %>.</h1>
+</body>
+</html>
